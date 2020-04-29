cf6292d98931bed6aed8a9e605bf5278b6e7c71e
techie97 <sagaroza038@gmail.com>
2020-04-29 13:20:09 -0700

    Few more updates

--- perm.v
+++ perm.v
@@ -1,68 +1,84 @@
+`timescale 1ns/1ps
+
+`include "theta.v"
+
 module perm(input clk, input reset, input [2:0] dix, input [199:0] din, input pushin, output [2:0] doutix, output [199:0] dout, output pushout);
 
-integer cnt;
-
-reg [1599:0] din_1600;
-reg [63:0] sha3_matrix [4:0] [4:0];
-integer x, y, z, s_index;
-reg [2:0]cnt_b;
-
-initial begin
-	cnt=0;
-	z=0;
-	x=0;
-	y=0;
-	s_index=0;
-end
-
-assign din_1600=({1400'b0,din}<<(dix*200));
-
-always@(posedge clk or posedge reset)
-begin
-	if(reset==1) begin
-		while(y<5) begin 
-			
-			sha3_matrix[x][y][z]<=0;
-			if(z>64) begin
-				z=0;
-				x=x+1;
-				if(x>5) begin
-					x=0;
-					y=y+1;
-				end
-			end
-			else z=z+1;
-		end
-		x=0;y=0;z=0;
-	end else begin
+	integer cnt;
 	
-		while( s_index<(((dix+1)*200)-1) ) begin
-		
-			s_index=((64*((5*y)+x))+z);
-			sha3_matrix[x][y][z]<=din_1600[s_index];
+	reg [1599:0] din_1600,din_buf;
+	reg [63:0] sha3_matrix [4:0] [4:0];
 
-			if(z>63) begin
-				z=0;
-				if(x>4)begin
-					x=0;
-					y=y+1;
-				end else x=x+1;
-				end	
-			else z=z+1;
+	reg [63:0] sha3_theta_m [4:0] [4:0];
+	integer x, y, z, s_index;
+	reg [2:0]cnt_b;
+	
+	initial begin
+		cnt<=0;	z<=0;	x<=0;	y<=0;	s_index<=0;
+	end
+	
+	assign din_1600=({1400'b0,din}<<(dix*200));
+	assign din_buf=(din_1600||din_buf);	
+	always@(posedge clk or negedge reset)
+	begin
+		if(!reset) begin
+			while(y<5) begin 
+				
+				sha3_matrix[x][y][z]=0;
+
+				if(z>63) begin
+					z=0;
+					if(x>4) begin
+						x=0;	y=y+1;
+					end else x=x+1;
+				end else z=z+1;
+			end
+
+			x=0;y=0;z=0;
+			s_index=0;
+
+		end else if(pushin) begin
+			while( s_index<(((dix+1)*200)-1) ) begin
+			
+				s_index=((64*((5*y)+x))+z);
+				sha3_matrix[x][y][z]=din_1600[s_index];
+
+				if(z>63) begin
+					z=0;
+					if(x>3)begin
+						x=0;	y=y+1;
+						if(y>4) begin y=0; end
+					end else x=x+1;
+				end else z=z+1;
+			end
 		end
 	end
-end
 
-always@(posedge clk or posedge reset)
-begin
-
-	$display("dix:%d",dix,"\tx:%d, y:%d, z:%d",x,y,z);
-	$display("%h",din);
-	$display("%h, %h, %h, %h, %h",sha3_matrix[0][0], sha3_matrix[1][0], sha3_matrix[2][0], sha3_matrix[3][0], sha3_matrix[4][0]);
-	$display("%h, %h, %h, %h, %h",sha3_matrix[0][1], sha3_matrix[1][1], sha3_matrix[2][1], sha3_matrix[3][1], sha3_matrix[4][1]);
-	$display("%h, %h, %h, %h, %h",sha3_matrix[0][2], sha3_matrix[1][2], sha3_matrix[2][2], sha3_matrix[3][2], sha3_matrix[4][2]);
-	$display("%h, %h, %h, %h, %h",sha3_matrix[0][3], sha3_matrix[1][3], sha3_matrix[2][3], sha3_matrix[3][3], sha3_matrix[4][3]);
-	$display("%h, %h, %h, %h, %h",sha3_matrix[0][4], sha3_matrix[1][4], sha3_matrix[2][4], sha3_matrix[3][4], sha3_matrix[4][4]);
+	sha3_theta 	theta3(clk, reset, pushin, sha3_matrix, sha3_theta_m);
 	
-end
+	always@(posedge clk)
+	begin
+		bit view_flag;
+		cnt=cnt+1;
+/*
+		$display("Clock cnt:%d\t\t\t ",cnt);
+		$monitor("dix:%d\t|",dix," x:%d\t| y:%d\t| z:%d\t",x,y,z);
+		$display("%h",din_buf); 
+*/
+	//	if(cnt==20 || cnt==3 ) begin
+		$display("#  0");
+		$display("# %h %h %h %h %h",sha3_matrix[0][0], sha3_matrix[1][0], sha3_matrix[2][0], sha3_matrix[3][0], sha3_matrix[4][0]); 
+		$display("# %h %h %h %h %h",sha3_matrix[0][1], sha3_matrix[1][1], sha3_matrix[2][1], sha3_matrix[3][1], sha3_matrix[4][1]); 
+		$display("# %h %h %h %h %h",sha3_matrix[0][2], sha3_matrix[1][2], sha3_matrix[2][2], sha3_matrix[3][2], sha3_matrix[4][2]); 
+		$display("# %h %h %h %h %h",sha3_matrix[0][3], sha3_matrix[1][3], sha3_matrix[2][3], sha3_matrix[3][3], sha3_matrix[4][3]); 
+		$display("# %h %h %h %h %h",sha3_matrix[0][4], sha3_matrix[1][4], sha3_matrix[2][4], sha3_matrix[3][4], sha3_matrix[4][4]); 
+
+		$display("# after theta 0");
+		$display("# %h, %h, %h, %h, %h",sha3_theta_m[0][0], sha3_theta_m[1][0], sha3_theta_m[2][0], sha3_theta_m[3][0], sha3_theta_m[4][0]);
+		$display("# %h, %h, %h, %h, %h",sha3_theta_m[0][1], sha3_theta_m[1][1], sha3_theta_m[2][1], sha3_theta_m[3][1], sha3_theta_m[4][1]);
+		$display("# %h, %h, %h, %h, %h",sha3_theta_m[0][2], sha3_theta_m[1][2], sha3_theta_m[2][2], sha3_theta_m[3][2], sha3_theta_m[4][2]);
+		$display("# %h, %h, %h, %h, %h",sha3_theta_m[0][3], sha3_theta_m[1][3], sha3_theta_m[2][3], sha3_theta_m[3][3], sha3_theta_m[4][3]);
+		$display("# %h, %h, %h, %h, %h",sha3_theta_m[0][4], sha3_theta_m[1][4], sha3_theta_m[2][4], sha3_theta_m[3][4], sha3_theta_m[4][4]);
+	//	end	
+	end
 endmodule
--- tb_perm.sv
+++ tb_perm.sv
@@ -1,3 +1,4 @@
+`timescale 1ns/1ps
 module perm_tb;
 
 reg clk, reset, pushin;
@@ -12,20 +13,55 @@ perm p0(.clk(clk), .reset(reset), .dix(dix), .din(din), .pushin(pushin), .doutix
 
 initial begin
 	clk=0;
-	forever #1 clk=~clk;
+	reset=1;
+//	dix=0;
+	forever #4 clk=~clk;
 end
 
 initial begin
-	reset=1;
+
 	#1 reset=0;
-	#2 dix=0; din=200'h60a636261;
-	#2 dix=1; din=200'h0;
-	#2 dix=2; din=200'h0;
-	#2 dix=3; din=200'h0;
-	#2 dix=4; din=200'h0;
-	#2 dix=5; din=200'h8000000000000000000000;
-	#2 dix=6; din=200'h0;
-	#2 dix=7; din=200'h0;
-	#3 $finish;
+	#1 reset=1;
+/*	#1 pushin=1; dix=0; din=200'h60a636261;
+	#8 dix=1; din=200'h0;
+	#8 dix=2; din=200'h0;
+	#8 dix=3; din=200'h0;
+	#8 dix=4; din=200'h0;
+	#8 dix=5; din=200'h8000000000000000000000;
+	#8 dix=6; din=200'h0;
+	#8 dix=7; din=200'h0;  
+	#16 pushin=0;
+*/
+ 
+//	#1 reset=0;
+//	#1 reset=1;
+	#1 pushin=1;
+	   dix=0; din=200'h5ffc9799a707e36d6008f0ebd4950cddd9c735df5ef7697fb9;
+	#8 dix=1; din=200'h95921de9ea6670d3da1f3ff4bb8cf703c9ff175e99412607ad;
+	#8 dix=2; din=200'h872756b6a20bb02edf0743040e1e30c9ed0248b16e2d5cabeb;
+	#8 dix=3; din=200'h5899495319322fed157cf9c617346b4501eaf614aad1426810;
+	#8 dix=4; din=200'h42e935b870e98d7c358a84c15cd868c2cf1d4a2b12a5f80a0a;
+	#8 dix=5; din=200'h17b703be0e667bf78a4d9f8f5ffb5ac96628c4381836f149f5;
+	#8 dix=6; din=200'h762a223c9f90c9ce97b5bdf073eed1118dc10e774520d780ca;
+	#8 dix=7; din=200'hacd51e6699f9823ce16682717c9bbfae76ab14759da618fd04;
+	#16 pushin=0;
+	
+/*	#1 pushin=1;
+	#8 dix=0; din=200'h7caa58695187b8a518d9a54a0d7b25331f83d9fca716c40a33;
+	#8 dix=1; din=200'hf3e00bc4621b9439852037106d208ead31c8a8486261790b2f;
+	#8 dix=2; din=200'hed49b12ac7e1da6099d795f1048aa604f0d0f2443658625af0;
+	#8 dix=3; din=200'h8212f7c6a7cda6c23fc86ee6d30aad4b45038e2206f1e77124;
+	#8 dix=4; din=200'he58e725d5758a295d4eff35b61138411eb0dde6d08121213ca;
+	#8 dix=5; din=200'hf9fe966844aa5560eaba017ad05133354fc7eefadf231025db;
+	#8 dix=6; din=200'h65e3eb74113cb04d6bfd8fa506bfc51c63373ac55ef1867f1f;
+	#8 dix=7; din=200'h879951fff4f991a81348486129fc1d9db5c36ec124ce01e1e0;
+	#8 pushin=0;
+*/	#200 $finish;
 end
+
+initial begin
+	$dumpvars(0);
+	$dumpfile("wave.vcd");
+end
+
 endmodule
